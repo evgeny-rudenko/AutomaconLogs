47323e686a590b45bd2352535c10c7ab6a5e2578
Евгений Руденко <e.rudenko@automacon.ru>
2023-04-11 14:14:35 +0300

    Добавьте файлы проекта.

---
+++ AutomaconLogs/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace AutomaconLogs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.rtbJSON = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.dataGridLog = new System.Windows.Forms.DataGridView();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnServers = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridLog)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 664);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1514, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // rtbJSON
+            // 
+            this.rtbJSON.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbJSON.Location = new System.Drawing.Point(1064, 39);
+            this.rtbJSON.Name = "rtbJSON";
+            this.rtbJSON.Size = new System.Drawing.Size(438, 622);
+            this.rtbJSON.TabIndex = 3;
+            this.rtbJSON.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(865, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(193, 108);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Выбрать данные";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // dataGridLog
+            // 
+            this.dataGridLog.AllowUserToAddRows = false;
+            this.dataGridLog.AllowUserToDeleteRows = false;
+            this.dataGridLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridLog.Location = new System.Drawing.Point(12, 121);
+            this.dataGridLog.Name = "dataGridLog";
+            this.dataGridLog.ReadOnly = true;
+            this.dataGridLog.Size = new System.Drawing.Size(1046, 540);
+            this.dataGridLog.TabIndex = 6;
+            this.dataGridLog.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridLog_CellClick);
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(32, 20);
+            this.toolStripSplitButton1.Text = "toolStripSplitButton1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(219, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(250, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(219, 39);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(250, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.DropDown += new System.EventHandler(this.comboBox2_DropDown);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(219, 67);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(250, 21);
+            this.comboBox3.TabIndex = 9;
+            this.comboBox3.DropDown += new System.EventHandler(this.comboBox3_DropDown);
+            this.comboBox3.DragDrop += new System.Windows.Forms.DragEventHandler(this.comboBox3_DragDrop);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Список баз на SRV-SQL20";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Список процедур";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Список параметров";
+            // 
+            // btnServers
+            // 
+            this.btnServers.Location = new System.Drawing.Point(490, 12);
+            this.btnServers.Name = "btnServers";
+            this.btnServers.Size = new System.Drawing.Size(120, 23);
+            this.btnServers.TabIndex = 13;
+            this.btnServers.Text = "Открыть базы";
+            this.btnServers.UseVisualStyleBackColor = true;
+            this.btnServers.Click += new System.EventHandler(this.btnServers_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(490, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Открыть процедуры";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(490, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Открыть параметры";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(490, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Открыть значения";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Значения параметров";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(219, 94);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(250, 21);
+            this.comboBox4.TabIndex = 19;
+            this.comboBox4.DropDown += new System.EventHandler(this.comboBox4_DropDown);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(1064, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(438, 29);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Копировать";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(695, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 108);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1514, 686);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnServers);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridLog);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.rtbJSON);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Анализ логов";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridLog)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.RichTextBox rtbJSON;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView dataGridLog;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnServers;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AutomaconLogs/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+
+
+namespace AutomaconLogs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnServers_Click(object sender, EventArgs e)
+        {
+            Process p = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo("Notepad.Exe", @"databases.txt");
+            p.StartInfo = psi;
+            p.Start();
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            string[] lineOfContents = File.ReadAllLines("databases.txt");
+            foreach (var line in lineOfContents)
+            {
+            
+                comboBox1.Items.Add(line);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process p = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo("Notepad.Exe", @"procedures.txt");
+            p.StartInfo = psi;
+            p.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process p = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo("Notepad.Exe", @"parameters.txt");
+            p.StartInfo = psi;
+            p.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process p = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo("Notepad.Exe", @"values.txt");
+            p.StartInfo = psi;
+            p.Start();
+        }
+
+        private void comboBox2_DropDown(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            string[] lineOfContents = File.ReadAllLines("procedures.txt");
+            foreach (var line in lineOfContents)
+            {
+
+                comboBox2.Items.Add(line);
+            }
+        }
+
+        private void comboBox3_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void comboBox3_DropDown(object sender, EventArgs e)
+        {
+            comboBox3.Items.Clear();
+            string[] lineOfContents = File.ReadAllLines("parameters.txt");
+            foreach (var line in lineOfContents)
+            {
+
+                comboBox3.Items.Add(line);
+            }
+        }
+
+        private void comboBox4_DropDown(object sender, EventArgs e)
+        {
+            comboBox4.Items.Clear();
+            string[] lineOfContents = File.ReadAllLines("values.txt");
+            foreach (var line in lineOfContents)
+            {
+
+                comboBox4.Items.Add(line);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //dataGridLog
+            // result.txt
+            // Определяем путь к файлу CSV
+            string filePath = @"result.txt";
+
+            // Определяем таблицу, в которую будем загружать данные
+            DataTable dataTable = new DataTable();
+
+            // Открываем файл CSV
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                // Получаем заголовки таблицы и добавляем их к таблице
+                string[] headers = streamReader.ReadLine().Split(';');
+                foreach (string header in headers)
+                {
+                    dataTable.Columns.Add(header);
+                }
+
+                // Читаем строки из файла CSV и добавляем их в таблицу
+                while (!streamReader.EndOfStream)
+                {
+                    string[] rows = streamReader.ReadLine().Split(';');
+                    DataRow dataRow = dataTable.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dataRow[i] = rows[i];
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
+
+            // загружаем данные в 
+            dataGridLog.DataSource = dataTable;
+
+
+        }
+
+        private void dataGridLog_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridLog.SelectedCells.Count > 0)
+            {
+                string value = dataGridLog.SelectedCells[0].Value.ToString();
+                // обработка значения ячейки
+
+                rtbJSON.Text = value;
+                               
+                value = value.Replace( "\"\"", "\"");
+                
+                
+                if (value.EndsWith("\"")&&value.StartsWith("\""))
+                {
+                    value = value.Substring(1, value.Length - 1);
+                    value = value.Substring(0, value.Length - 1);
+
+                    value = value;
+                    
+                }
+
+                JObject json;
+                try
+                {
+                    json = JObject.Parse(value);
+                    string formattedJsonString = JToken.Parse(JsonConvert.SerializeObject(json)).ToString();
+
+                    rtbJSON.Text = formattedJsonString;
+
+                }
+                catch (Exception ex)
+                {
+                    rtbJSON.Text = value;
+
+                }
+
+            }
+
+        }
+    }
+}
